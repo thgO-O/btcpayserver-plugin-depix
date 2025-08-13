39514154b9fb695e7fec69f6be5cbcfda162305d
thgO.O <thgoil@proton.me>
2025-08-13 20:00:56 -0300

    feat: extend PixStoreViewModel for one-time secret + regen

--- BTCPayServer.Plugins.Depix/Data/Models/ViewModels/PixStoreViewModel.cs
+++ BTCPayServer.Plugins.Depix/Data/Models/ViewModels/PixStoreViewModel.cs
@@ -1,11 +1,13 @@
+#nullable enable
 namespace BTCPayServer.Plugins.Depix.Data.Models.ViewModels;
 
 public class PixStoreViewModel
 {
     public bool Enabled { get; set; }
-    public string ApiKey { get; set; }
-    
-    public string WebhookUrl { get; set; } 
-    public string WebhookSecretHex { get; set; } // 64 chars (32 bytes hex)
-    public string TelegramRegisterCommand { get; set; }
+    public string? ApiKey { get; set; }
+    public string? WebhookUrl { get; set; } 
+    public string? WebhookSecretDisplay { get; set; }
+    public bool RegenerateWebhookSecret { get; set; }
+    public string? OneShotSecretToDisplay { get; set; }
+    public string? TelegramRegisterCommand { get; set; }
 }
